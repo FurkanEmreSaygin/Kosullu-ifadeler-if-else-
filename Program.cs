c55437094bc03c2453cb27e374432ed48101d00f
Furkan Emre Saygin <furkanemresaygin@gmail.com>
2025-02-28 12:23:14 +0300

    Koşul oluşturuldu

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program{
+
+    public static void cookie(int i){
+        if(i>0){
+            Console.WriteLine("Your number is positive.");
+        }
+        else if(i<0){
+            Console.WriteLine("Your number is negative.");
+        }
+        else{
+            Console.WriteLine("Your number is 0. ");
+        }
+
+
+    }
+    static void Main(){
+
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        cookie(number);
+
+    }
+}
